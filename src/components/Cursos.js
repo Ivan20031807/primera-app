d808efb457ed117078fd1cff22bfc6cf962424ce
Ivan20031807 <84926299+Ivan20031807@users.noreply.github.com>
2022-02-14 09:44:45 -0500

    Tarea

---
+++ src/components/Cursos.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Cursoform from './Cursoform';
+
+
+export default function Cursos({cursos, onEliminarCursos}) {
+    const recibir=()=>{
+        var rec = document.getElementById("id").value;
+        document.getElementById("escribir").innerHTML= "EL curso nuevo es :"+rec+""
+    }
+    const eliminarCursoHandler = (id) =>{
+        console.log("Eliminando curso..", id);
+        onEliminarCursos(id);
+    }
+    
+  return <div>
+      <h2>
+          Listado de Cursos
+      </h2>
+      <hr/>
+            <Cursoform/>
+            <div id='escribir'>
+                <button onClick={recibir}>
+                    Agregar
+                </button>
+            </div>
+        <hr/>
+      <ul>  
+          {
+              cursos.map(item =>(
+                  <li key={item.id} >{item.id} - {item.Cursos}
+                  <br></br>
+                    <button className='btn btn-warning' onClick={ ()=>eliminarCursoHandler(item.id)} >Eliminar curso  </button>
+                  </li>
+              ))
+          }
+      </ul>
+  </div>;
+}
